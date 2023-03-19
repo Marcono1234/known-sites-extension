65274dc22b9aba3f5459f75e2e3e53dbcd374818
Marcono1234 <Marcono1234@users.noreply.github.com>
2023-03-19 17:22:08 +0100

    Fix inconsistent newlines in source maps
    Fixes #1

--- typescript-src/webpack.config.js
+++ typescript-src/webpack.config.js
@@ -1,36 +1,38 @@
 // Based on https://webpack.js.org/guides/typescript/
 
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './background-scripts/background.ts',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'background.js',
-        path: path.resolve(__dirname, '..', 'extension', 'background-scripts'),
-    },
-    plugins: [
-        // Include polyfill to support Chrome, see https://github.com/mozilla/webextension-polyfill#usage-with-webpack-without-bundling
-        // TODO: Maybe move this task to top-level folder of this project because it is not specific to TypeScript sources
-        new CopyWebpackPlugin({
-            patterns: [{
-                from: 'node_modules/webextension-polyfill/dist/browser-polyfill.js',
-                to: path.resolve(__dirname, '..', 'extension', 'web-ext-polyfill')
-            }],
-        })
-    ]
-};
+  mode: 'development',
+  entry: './background-scripts/background.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'background.js',
+    path: path.resolve(__dirname, '..', 'extension', 'background-scripts'),
+  },
+  plugins: [
+    // Include polyfill to support Chrome, see https://github.com/mozilla/webextension-polyfill#usage-with-webpack-without-bundling
+    // TODO: Maybe move this task to top-level folder of this project because it is not specific to TypeScript sources
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'node_modules/webextension-polyfill/dist/browser-polyfill.js',
+          to: path.resolve(__dirname, '..', 'extension', 'web-ext-polyfill'),
+        },
+      ],
+    }),
+  ],
+}
