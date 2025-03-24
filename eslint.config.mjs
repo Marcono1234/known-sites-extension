a7bbbdadc62c9a5283d5e08f215da5dc231cb47b
Marcono1234 <Marcono1234@users.noreply.github.com>
2025-03-24 23:55:02 +0100

    Configure Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,18 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default tseslint.config([
   // Ignore generated JavaScript files
   globalIgnores(['extension/**/*.js']),
   {
     files: ['typescript-src/**/*.ts'],
-    extends: [eslint.configs.recommended, tseslint.configs.recommended],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      // Disable rules which conflict with Prettier
+      eslintConfigPrettier,
+    ],
   },
 ])
