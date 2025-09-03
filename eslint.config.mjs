e52200b13e45b878c5e395bd085a90efb0a8cfae
Marcono1234 <Marcono1234@users.noreply.github.com>
2025-09-03 23:55:12 +0200

    Replace deprecated `tseslint.config`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,10 @@
 
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
-export default tseslint.config([
+export default defineConfig([
   // Ignore generated JavaScript files
   globalIgnores(['extension/**/*.js']),
   {
